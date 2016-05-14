b3977bc3e43bab3dd0ea1cb2cb93abcfbf7745e3
Tim Schumacher <tim@timakro.de>
2016-05-14 14:47:12 +0200

    delete different gametypes

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -9,10 +9,7 @@
 #include <game/version.h>
 #include <game/collision.h>
 #include <game/gamecore.h>
-#include "gamemodes/dm.h"
-#include "gamemodes/tdm.h"
-#include "gamemodes/ctf.h"
-#include "gamemodes/mod.h"
+#include "gamecontroller.h"
 
 enum
 {
@@ -1488,15 +1485,7 @@ void CGameContext::OnInit(/*class IKernel *pKernel*/)
 	//world = new GAMEWORLD;
 	//players = new CPlayer[MAX_CLIENTS];
 
-	// select gametype
-	if(str_comp(g_Config.m_SvGametype, "mod") == 0)
-		m_pController = new CGameControllerMOD(this);
-	else if(str_comp(g_Config.m_SvGametype, "ctf") == 0)
-		m_pController = new CGameControllerCTF(this);
-	else if(str_comp(g_Config.m_SvGametype, "tdm") == 0)
-		m_pController = new CGameControllerTDM(this);
-	else
-		m_pController = new CGameControllerDM(this);
+	m_pController = new IGameController(this);
 
 	// setup core world
 	//for(int i = 0; i < MAX_CLIENTS; i++)
